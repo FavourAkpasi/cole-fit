0802e0a329849099307f4fd55a7fd9aeac938671
favourakpasi <akpasifavour@gmail.com>
2022-07-21 03:17:04 +0100

    picture bug

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,7 +5,10 @@ const Main = () => {
   return (
     <main>
       <section className="banner">
-        <img src="/images/cole2.JPG" alt="main" />
+        <img src="/images/cole2.jpg" alt="main" />
+      </section>
+      <section className="banner">
+        <img src="/images/fave.jpeg" alt="fave" />
       </section>
     </main>
   );
